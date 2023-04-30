ee5f19f4a9d87bdf8c9fb3a04905f943f798fbb1
Ftoupet <2132122@cmontmorency.qc.ca>
2023-04-30 00:28:46 -0400

    Logo + Titre Onglet

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,43 @@
-// import logo from './logo.svg';
 import './App.css';
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import Navbar from "./Navigation/Navbar.js";
-
-// function App() {
-//   return (
-//     <div>
-//       {/* <Navbar /> */}
-//     </div>
-//   );
-// }
-
-// export default App;
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleToggle = (isOpen) => {
+    setIsActive(isOpen);
+  };
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="light" expand="lg" onToggle={handleToggle}>
         <Navbar.Brand href="#Acceuil">
           <img
             src="https://i.imgur.com/a834LGK.png"
             width="50"
             className='imgLogoNavbar'
-            // height="30"
             alt="Logo"
           />{' '}
         </Navbar.Brand>
-        <div className='StageMomoTitle'>
+        <div className='StageMomoTitle title-with-border'>
           Stages Montmorency
           </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#Acceuil">Acceuil</Nav.Link>
-            <Nav.Link href="#employeurs">Espace Employeurs</Nav.Link>
-            <Nav.Link href="#employeurs">Espace Stagiaires</Nav.Link>
-            <NavDropdown title="Autres" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">FAQ</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Coordonnateurs</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">College Montmorency</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Equipe de developpement</NavDropdown.Item>
+            <Nav.Link className="navElements" href="#Acceuil">Acceuil</Nav.Link>
+            <Nav.Link className="navElements" href="#Employeurs">Espace Employeurs</Nav.Link>
+            <Nav.Link className="navElements" href="#Stagiaires">Espace Stagiaires</Nav.Link>
+            <NavDropdown className="navElements" style={{color:"red"}} title="Autres" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#FAQ">FAQ</NavDropdown.Item>
+              <NavDropdown.Item href="#Coordonnateurs">Coordonnateurs</NavDropdown.Item>
+              <NavDropdown.Item href="https://www.cmontmorency.qc.ca/" target="_blank" >College Montmorency ↗</NavDropdown.Item>
+              <NavDropdown.Divider className='Divider'/>
+              <NavDropdown.Item href="#DevTeam">Equipe de developpement</NavDropdown.Item>
             </NavDropdown>
           </Nav>
-          <div className='divImgProfilNavbar'>
+          <div className={isActive ? "divImgProfilNavbarActive" : "divImgProfilNavbar"}>
             <img
               src="https://i.imgur.com/gGYpde6.png"
               className="ImgProfilNavBar"
@@ -64,3 +55,4 @@ function App() {
 }
 
 export default App;
+// https://www.cmontmorency.qc.ca/wp-content/uploads/2023/04/Accueil-nouveau-site-web-1-992x365.png
