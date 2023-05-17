ef3a1bdb1f3e9ac7a39f35e221cf877167b856c2
Brandon Myre <2212060@cmontmorency.qc.ca>
2023-05-17 19:42:54 -0400

    fixLeMergeAFlorentin

--- client/src/components/StageAjout/StageAjout.js
+++ client/src/components/StageAjout/StageAjout.js
@@ -4,51 +4,6 @@ function StageAjout() {
 
 
   return (
-<<<<<<< Updated upstream
-    <div className="StageAjout-Main">
-        <form>
-          <label className="StageAjout-Label" for="titre">Titre:</label>
-          <input className="StageAjout-Input" type="text" id="titre" name="titre"></input>
-          <br/>
-          <label className="StageAjout-Label" for="prenom">Prenom Contact:</label>
-          <input className="StageAjout-Input" type="text" id="prenom" name="prenom"></input>
-          <br/>
-          <label className="StageAjout-Label" for="nom">Nom Contact:</label>
-          <input className="StageAjout-Input" type="text" id="nom" name="nom"></input>
-          <br/>
-          <label className="StageAjout-Label" for="courriel">Courriel:</label>
-          <input className="StageAjout-Input" type="email" id="courriel" name="courriel"></input>
-          <br/>
-          <label className="StageAjout-Label" for="numeroCell">Telephone:</label>
-          <input className="StageAjout-Input" type="tel" id="numeroCell" name="numeroCell"></input>
-          <br/>
-          <label className="StageAjout-Label" for="nomEntreprise">Nom Entreprise:</label>
-          <input className="StageAjout-Input" type="text" id="nomEntreprise" name="nomEntreprise"></input>
-          <br/>
-          <label className="StageAjout-Label" for="adresseEntreprise">Adresse Entreprise:</label>
-          <input className="StageAjout-Input" type="text" id="adresseEntreprise" name="adresseEntreprise"></input>
-          <br/>
-          <label>Type:</label>
-          <select id="type">
-                    <option value="Types.Reseaux">Reseaux</option>
-                    <option value="Types.Developpement">Developpement</option>
-          </select>
-          <br/>
-          <label className="StageAjout-Label" for="nomEntreprise">Nom Entreprise:</label>
-          <input className="StageAjout-Input" type="text" id="nomEntreprise" name="nomEntreprise"></input>
-          <br/>
-          <label className="StageAjout-Label" for="nbPoste">Nombre de Poste:</label>
-          <input className="StageAjout-Input" type="number" id="nbPoste" name="nbPoste"></input>
-          <br/>
-          <label className="StageAjout-Label" for="description">Description:</label>
-          <input className="StageAjout-Input" type="text" id="description" name="description"></input>
-        </form>
-    </div>
-  );
-}
-
-export default StageAjout;
-=======
     <div className="spacing">
     <div className="StageAjout-Container">
     <div className="StageAjout-Title">
@@ -204,7 +159,7 @@ export default StageAjout;
 
 export default StageAjout;
 
-    {/* // <div className="spacing">
+    /* // <div className="spacing">
     // <div className="StageAjout-Container">
     //   <div className="StageAjout-Title">
     //     <h2>Ajouter un stage</h2>
@@ -261,5 +216,4 @@ export default StageAjout;
     // </div>
     // </div>
 
-// export default StageAjout; */}
->>>>>>> Stashed changes
+// export default StageAjout; */
