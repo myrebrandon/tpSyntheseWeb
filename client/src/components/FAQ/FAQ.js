05288915c1a13d93fbfc59c9fbe131d8ddb96a71
Ftoupet <2132122@cmontmorency.qc.ca>
2023-05-20 15:59:20 -0400

    Page FAQ complete

--- client/src/components/FAQ/FAQ.js
+++ client/src/components/FAQ/FAQ.js
@@ -1,36 +1,46 @@
-import listeFAQ from "../../data/listefAQ"
-export default function FAQ() {
-    return (
-        <div class="container" className="listeFAQ-Main">
-            <h2>Frequently Asked Questions</h2>
-                <div class="accordion">
-                {
-                    listeFAQ.map((element, cle) => (
-                    <div className="accordion-item" key={cle}>
-                    <div class="accordion-item">
-                        <button id="accordion-button-1" aria-expanded="false">
-                            <span class="accordion-title">Question : {element.question}</span>
-                            <span class="icon" aria-hidden="true"></span>
-                        </button>
-                        <div class="accordion-content">
-                            <p>Reponse : {element.reponse}</p>
-                        </div>
-                    
-                    </div>
-                    </div>
-                }
-                </div>
-        </div>
+import listeFAQ from "../../data/listefAQ";
+import './FAQ.css';
+import React, { useEffect } from 'react';
 
-        // <div className="listeFAQ-Main">
-        //     {
-        //         listeFAQ.map((element, cle) => (
-        //             <div className="listeFAQ-Question" key={cle}>
-        //                 <h1> Question : {element.question} </h1>
-        //                 <p> Reponse : {element.reponse} </p>
-        //             </div>
-        //         ))
-        //     }
-        // </div>
-    )
+export default function FAQ() {
+    useEffect(() => {
+        const items = document.querySelectorAll('.FAQ-accordion .FAQ-button');
+
+        function toggleAccordion() {
+            const itemToggle = this.getAttribute('aria-expanded');
+
+            items.forEach((item) => {
+                item.setAttribute('aria-expanded', 'false');
+            });
+
+            if (itemToggle === 'false') {
+                this.setAttribute('aria-expanded', 'true');
+            }
+        }
+
+        items.forEach((item) => item.addEventListener('click', toggleAccordion));
+
+        return () => {
+            items.forEach((item) => item.removeEventListener('click', toggleAccordion));
+        };
+    }, []);
+
+    return (
+        <div className="container FAQ-listeFAQ-Main">
+            <h2 className="FAQ-h2">Frequently Asked Questions</h2>
+            <div className="FAQ-accordion">
+                {listeFAQ.map((element, cle) => (
+                    <div className="FAQ-accordion-item" key={cle}>
+                        <button id={`accordion-button-${cle}`} className="FAQ-button" aria-expanded="false">
+                            <span className="FAQ-accordion-title">{element.question}</span>
+                            <span className="FAQ-icon" aria-hidden="true"></span>
+                        </button>
+                        <div className="FAQ-accordion-content">
+                            <p className="FAQ-accordion-p">{element.reponse}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
