b45763a2677177ccacbe61e1ce35aab5c6c84d5d
Ftoupet <2132122@cmontmorency.qc.ca>
2023-05-20 15:34:46 -0400

    FAQ Style

--- client/src/components/FAQ/FAQ.js
+++ client/src/components/FAQ/FAQ.js
@@ -1,15 +1,36 @@
 import listeFAQ from "../../data/listefAQ"
 export default function FAQ() {
     return (
-        <div className="listeFAQ-Main">
-            {
-                listeFAQ.map((element, cle) => (
-                    <div className="listeFAQ-Question" key={cle}>
-                        <h1> Question : {element.question} </h1>
-                        <p> Reponse : {element.reponse} </p>
+        <div class="container" className="listeFAQ-Main">
+            <h2>Frequently Asked Questions</h2>
+                <div class="accordion">
+                {
+                    listeFAQ.map((element, cle) => (
+                    <div className="accordion-item" key={cle}>
+                    <div class="accordion-item">
+                        <button id="accordion-button-1" aria-expanded="false">
+                            <span class="accordion-title">Question : {element.question}</span>
+                            <span class="icon" aria-hidden="true"></span>
+                        </button>
+                        <div class="accordion-content">
+                            <p>Reponse : {element.reponse}</p>
+                        </div>
+                    
                     </div>
-                ))
-            }
+                    </div>
+                }
+                </div>
         </div>
+
+        // <div className="listeFAQ-Main">
+        //     {
+        //         listeFAQ.map((element, cle) => (
+        //             <div className="listeFAQ-Question" key={cle}>
+        //                 <h1> Question : {element.question} </h1>
+        //                 <p> Reponse : {element.reponse} </p>
+        //             </div>
+        //         ))
+        //     }
+        // </div>
     )
 }
