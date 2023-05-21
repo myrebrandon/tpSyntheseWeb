9f36331ac5d2dc3e9d9bb3e04959e8d727eaca42
Ftoupet <2132122@cmontmorency.qc.ca>
2023-05-21 17:14:39 -0400

    Fuck Margin

--- client/src/components/StageCard/StageCard.js
+++ client/src/components/StageCard/StageCard.js
@@ -1,49 +1,36 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './StageCard.css';
 
-
-
-
-function StageCard( { info, idEtudiant } ){
+function StageCard({ info, idEtudiant }) {
   const imageSrc = info.type === 'Developpement d\'application'
     ? 'https://cdn-icons-png.flaticon.com/512/2809/2809263.png'
     : 'https://cdn-icons-png.flaticon.com/128/4379/4379213.png';
-        // <div className="stage-item__content">
-        //   <h2>{info.titre}</h2>
-        //   <div className="stage-item__info">
-        //     <h3>{info.type}</h3>
-        //     <p>{info.nomEntreprise}</p>
-        //     {!idEtudiant ? <Link to={`/stage/${info._id}`}>Info Stage</Link> :
-        //      <Link to={`/${idEtudiant}/Affectation/${info._id}`}>Info Stage</Link>}
-        //   </div>
-        // </div>
 
   return (
-        <div className='stageCard-Main'>
-          <article class="job-card">
-            <div class="company-logo-img">
-              <img src={imageSrc} alt="Stage Type"/>
-            </div>
-            <div class="job-title">{info.titre}</div>
-            <div class="company-name">{info.type}</div>
-            <div class="skills-container">
-              <div class="skill">{info.nomEntreprise}</div>
-              <div class="skill">{info.nbPostes} poste(s) à combler</div>
-              <div class="skill">{info.etat}</div>
-            </div>
-            {!idEtudiant ? <Link to={`/stage/${info._id}`}>Info Stage</Link> :
-            <Link to={`/${idEtudiant}/Affectation/${info._id}`}>Info Stage</Link>}
-        <button class="apply">Apply</button>
-        <button class="save">Save Job</button>
+    <div className='stageCard-Main'>
+      <article className="job-card">
+        <div className="company-logo-img">
+          <img src={imageSrc} alt="Stage Type" />
+        </div>
+        <div className="job-title">{info.titre}</div>
+        <div className="company-name">{info.type}</div>
+        <div className="skills-container">
+          <div className="skill">{info.nomEntreprise}</div>
+          <div className="skill">{info.nbPostes} poste(s) à combler</div>
+          <div className="skill">{info.etat}</div>
+        </div>
+        {!idEtudiant ? 
+          <Link className='apply button-apply' to={`/stage/${info._id}`}>Info Stage</Link> :
+          <Link className='apply button-apply' to={`/${idEtudiant}/Affectation/${info._id}`}>Info Stage</Link>}
+          {/* <div className='div-apply'>
+            <button className="apply button-apply">Appliquer</button>
+          </div> */}
+        {/* <button className="save">Save Job</button> */}
         <a href="#"></a>
-        </article>
-      </div>
+      </article>
+    </div>
   );
-};
-export default StageCard;
+}
 
-{/* <script
-  src="http://code.jquery.com/jquery-3.2.1.min.js"
-  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-  crossorigin="anonymous"></script> */}
+export default StageCard;
