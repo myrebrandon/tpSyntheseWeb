6e859f1b7de68017415c0e4586e36a71a3f82df1
Antonii Spasov <2054266@cmontmorency.qc.ca>
2023-05-21 19:40:35 -0400

    style basique acueill

--- client/src/components/Accueil/Accueil.js
+++ client/src/components/Accueil/Accueil.js
@@ -5,38 +5,42 @@ export default function Accueil() {
     return (
         <div className="accueil-container">
             <div className="accueil-header">
-                <h1 className="accueil-title">Édition {date}</h1>
+                <div className="accueil-header-title">
+                    <h2>Bienvenue sur le site des stages de fin d'études des techniques de l'informatique du Collège Montmorency!</h2>
+                </div>
+                <img className="accueil-header-logo" src={"/images/Accueil.png"} alt='image'></img>
+                <div className="accueil-header-subtitle">
+                    <h3>À la fin de leurs études, les étudiants sont appelés à mettre en pratique les compétences acquises durant le programme. Cela se fait grâce à la participation d'entreprises de la région qui les accueillent afin de finaliser leurs formations.</h3>
+                </div>
             </div>
 
             <div className="accueil-content">
-                <div className="accueil-section">
-                    <h2 className="accueil-section-title">Bienvenue sur le site des stages de fin d'études des techniques de l'informatique du Collège Montmorency!</h2>
-                </div>
+                <div>
+                    <div className='accueil-content-titre'>
+                        <h1>Édition {date} </h1>
+                    </div>
 
-                <div className="accueil-section">
-                    <h3 className="accueil-section-subtitle">À la fin de leurs études, les étudiants sont appelés à mettre en pratique les compétences acquises durant le programme. Cela se fait grâce à la participation d'entreprises de la région qui les accueillent afin de finaliser leurs formations.</h3>
-                </div>
+                    <div className="accueil-section">
+                        <p className="accueil-paragraph">
+                            Le Collège Montmorency offre ainsi aux employeurs l'occasion d'obtenir une main-d'œuvre compétente, tout en leur permettant de participer à la formation finale des étudiants.
+                        </p>
+                    </div>
 
-                <div className="accueil-section">
-                    <p className="accueil-paragraph">
-                        Le Collège Montmorency offre ainsi aux employeurs l'occasion d'obtenir une main-d'œuvre compétente, tout en leur permettant de participer à la formation finale des étudiants.
-                    </p>
-                </div>
+                    <div className="accueil-section">
+                        <p className="accueil-paragraph">
+                            Le stage de fin d'études est une expérience concrète permettant d'acquérir une expérience professionnelle formatrice.
+                        </p>
+                    </div>
 
-                <div className="accueil-section">
-                    <p className="accueil-paragraph">
-                        Le stage de fin d'études est une expérience concrète permettant d'acquérir une expérience professionnelle formatrice.
-                    </p>
-                </div>
-
-                <div className="accueil-section">
-                    <p className="accueil-paragraph">
-                        Les étudiants terminent la portion académique de leurs études en informatique selon une des deux voies de sortie du programme:
-                    </p>
-                    <ul className="accueil-list">
-                        <li>Réseaux et sécurité informatique</li>
-                        <li>Développement d'applications informatiques</li>
-                    </ul>
+                    <div className="accueil-section">
+                        <p className="accueil-paragraph">
+                            Les étudiants terminent la portion académique de leurs études en informatique selon une des deux voies de sortie du programme:
+                        </p>
+                        <ul className="accueil-list">
+                            <li>Réseaux et sécurité informatique</li>
+                            <li>Développement d'applications informatiques</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
--- client/src/components/Loading/Loading.js
+++ client/src/components/Loading/Loading.js
@@ -1,7 +1,5 @@
 export default function Loading() {    
     return ( 
-        <div>
-            <h2>Loading</h2>
-        </div>
+        <div className="loading"></div>
     )
 }
